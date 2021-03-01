cd1a89d072e43676bc910798a1e54d783e3f69dd
Bryzzen <xdbrgames10@gmail.com>
2021-03-01 18:25:22 -0300

    v1

--- Extra/GuildCache.ts
+++ Extra/GuildCache.ts
@@ -2,7 +2,7 @@ import { APIMessage, Structures } from "discord.js";
 import gDb from "../Modelos/guild"
 
 class Guild extends Structures.get("Guild"){ // Copy D4rk
-    async getCache(){
+    async guildCache(){
         let guild = await gDb.findOne({id: this.id})
 
         if(guild){
