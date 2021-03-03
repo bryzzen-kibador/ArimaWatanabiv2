42059b1c9e5522cff8738f3dc42719f3d9d204ef
Bryzzen <xdbrgames10@gmail.com>
2021-03-03 08:06:38 -0300

    v1

--- Comandos/skip.ts
+++ Comandos/skip.ts
@@ -1,7 +1,6 @@
 import { Message } from "discord.js"
 import Command from "../Estruturas/Command"
 import Client from "../Estruturas/Client"
-import { Player } from "erela.js"
 
 module.exports = class Play extends Command {
 
@@ -12,6 +11,7 @@ module.exports = class Play extends Command {
             name: "skip",
             aliases: ["pular"],
             category: "musica",
+            usage: { en: `[Amount]`, pt: "[Quantia]" }
         })
         this.client = client
     }
@@ -24,11 +24,52 @@ module.exports = class Play extends Command {
 
         if (!voice?.channel) return message.channel.send(trans.voiceChannel)
 
-        const player = this.client.music.players.get(message.guild?.id as string)
+        const player = this.client.music?.players.get(message.guild?.id as string)
 
         if (player && player.voiceChannel !== voice.channel.id) return message.channel.send(trans.myVoiceChannel)
 
         try {
+
+            if (args[0]) {
+                let quantia = args[0]
+                if (!quantia || isNaN(parseInt(quantia)) || parseInt(quantia) <= 0 || parseInt(quantia) == Infinity) return message.channel.send(message.guild.guildCache?.lang == "pt" ? `❌ Número Inválido` : `❌ Invalid Number`)
+                let quant = parseInt(quantia)
+                if (message.author == player?.queue.current?.requester || voice.channel.members.filter(f => !f.user.bor).size == 1) {
+                    player?.stop(quant)
+
+                    if (!player?.queue[0]) {
+                        player?.destroy()
+                        return message.channel.send(message.guild.guildCache?.lang == "pt" ? `🎶 A lista de música acabou!` : `🎶 The queue is end!`).then(msg => msg.delete({ timeout: 5000 }))
+                    }
+
+                    return message.channel.send(message.guild.guildCache?.lang == "pt" ? `🎶 Música Pulada!` : `🎶 Skipped!`)
+                } else {
+                    if (message.guild.guidCache?.dj) {
+                        if (await this.client.music?.hasDj(message.member)) {
+                            player?.stop(quant)
+
+                            if (!player?.queue[0]) {
+                                player?.destroy()
+                                return message.channel.send(message.guild.guildCache?.lang == "pt" ? `🎶 A lista de música acabou!` : `🎶 The queue is end!`).then(msg => msg.delete({ timeout: 5000 }))
+                            }
+
+                            return message.channel.send(message.guild.guildCache?.lang == "pt" ? `🎶 Música Pulada!` : `🎶 Skipped!`)
+                        } else {
+                            return message.channel.send(message.guild.guildCache?.lang == "pt" ? `❌ Apenas um DJ ou quem requisitou a música pode pula-lá!` : `❌ Only a DJ or whoever ordered the music can skip it!`).then(msg => msg.delete({ timeout: 5000 }))
+                        }
+                    } else {
+                        player?.stop(quant)
+
+                        if (!player?.queue[0]) {
+                            player?.destroy()
+                            return message.channel.send(message.guild.guildCache?.lang == "pt" ? `🎶 A lista de música acabou!` : `🎶 The queue is end!`).then(msg => msg.delete({ timeout: 5000 }))
+                        }
+
+                        return message.channel.send(message.guild.guildCache?.lang == "pt" ? `🎶 Música Pulada!` : `🎶 Skipped!`).then(msg => msg.delete({ timeout: 5000 }))
+
+                    }
+                }
+            }
 
             if (!player?.playing) return message.channel.send(await this.client.getTranslate(message.guild?.id as string, "stopP"))
             if (message.guild.guildCache?.dj) {
