f829ca66b07aae9e7879653046011e08aafe45e8
Bryzzen <34305708+bryzzen-kibador@users.noreply.github.com>
2021-03-04 16:17:55 -0300

    Update lavalink.ts

--- Comandos/lavalink.ts
+++ Comandos/lavalink.ts
@@ -20,7 +20,7 @@ module.exports = class Lavalink extends Command {
         let uptime = this.client.utils.mstodate(node?.uptime as number)
         let embed = new this.client.utils.embed()
             .setTitle(`🎶 Lavalink \`${this.client.music?.nodes.first()?.options.identifier}\``)
-            .addField(`<:load:488757308248293396> Players:`, `\`` + node?.players + `\``)
+            .addField(`<:lab_poarr:689926691342385198> Players:`, `\`` + node?.players + `\``)
             .addField(`<:lab_daora:568569334134472734> Uptime:`, `${uptime.days}D/${uptime.hours}H/${uptime.minutes}M`)
             .addField(`<:lab_on:811281161892265995> RAM:`, `${Math.floor(node?.memory.used / 1024 / 1024)}/${Math.floor(node?.memory.reservable / 1024 / 1024)}MB`)
             .addField(`<:lab_selfie:783901503395069962> CPU:`, `
