c0f89ce48c707925916227b8413fde1211f9d527
Bryzzen <34305708+bryzzen-kibador@users.noreply.github.com>
2021-03-04 16:31:52 -0300

    Update guild.ts

--- Modelos/guild.ts
+++ Modelos/guild.ts
@@ -1,14 +1,12 @@
 import {Schema, model, Document} from "mongoose"
 
 interface Guild extends Document {
-    _id: string;
     prefix?: string;
     lang: string;
     dj: string;
 }
 
 const guild = new Schema({
-    _id: String,
     prefix: String,
     lang: String,
     dj: String
