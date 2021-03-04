590157c3e33fe40cbf9a222b1c375de4297c56eb
Bryzzen <34305708+bryzzen-kibador@users.noreply.github.com>
2021-03-04 16:08:37 -0300

    Update ready.ts

--- Eventos/ready.ts
+++ Eventos/ready.ts
@@ -9,6 +9,7 @@ module.exports = class Ready{
 
     async execute(){
         await this.client.loadGuilds()
+        await this.client.status()
         console.log(this.client.user?.username + " Iniciado com sucesso!")
     }
 }
