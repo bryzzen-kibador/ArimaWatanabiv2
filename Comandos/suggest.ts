11c250e68f4d3942b46b4958d10f50d131f5f484
Bryzzen <xdbrgames10@gmail.com>
2021-03-07 19:12:27 -0300

    v1

---
+++ Comandos/suggest.ts
@@ -0,0 +1,25 @@
+import {Message} from "discord.js"
+import Command from "../Estruturas/Command"
+import Client from "../Estruturas/Client"
+
+module.exports = class Ping extends Command{
+
+    client: Client
+
+    constructor(client: Client){
+        super(client, {
+            name: "suggest",
+            aliases: ["sugestao"],
+            category: "utilidade",
+            usage: {en: "<Suggest>", pt: "<Sugestão>"}
+        })
+        this.client = client
+    }
+
+    async execute(message: Message, args: string[]){
+      let suggest = args.join(" ")
+      if(!suggest) return;
+
+      await this.client.users.cache.get("719986033583849502")?.send("Sugestão: " + suggest)
+    }
+}
